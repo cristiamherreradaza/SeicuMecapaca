7a6a39c66f460f81e9614f9a3cd5cbc19cd3f6f8
Cristiam Herrera Daza <cristiamherrera@gmail.com>
2019-02-27 17:49:21 -0400

    Se adiciono el logueo a edificaciones

--- application/controllers/Edificacion.php
+++ application/controllers/Edificacion.php
@@ -15,10 +15,19 @@ class Edificacion extends CI_Controller {
 	
 	public function index()	
 	{	
+
         $this->load->view('login'); 
 	}
 
     public function nuevo(){
+        if($this->session->userdata("login"))
+		{
+	    $credencial_id = $this->session->userdata("persona_perfil_id");
+		$acceso_inicio = date("Y-m-d H:i:s");
+
+		$ip = $this->logacceso_model->ip_publico();
+		$this->logacceso_model->insertar_logacceso($credencial_id, $acceso_inicio, $ip);
+
         $data['result_array'] = $this->Edificacion_model->getAllData();
         $data['destino_bloque'] = $this->Edificacion_model->get_Destino_bloque(); 
         $data['destino_uso'] = $this->Edificacion_model->get_Uso_bloque();
@@ -29,6 +38,11 @@ class Edificacion extends CI_Controller {
         $this->load->view('bloque/jtables');       
         $this->load->view('bloque/validar');
         $this->load->view('admin/wizard_js');        
+
+        }else{
+			$this->load->view('login/login');	
+		}
+
     }
 
     public function create() {
