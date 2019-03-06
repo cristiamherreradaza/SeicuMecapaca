3f5c9238a72ca3fd5a8ad13bc66af427223d2e90
Cristiam Herrera Daza <cristiamherrera@gmail.com>
2019-03-06 18:30:13 -0400

    Se termino de modificar la vista y se esta corrigiendo algunos problemas en la vista de registro de nuevo predio

--- application/controllers/Predios.php
+++ application/controllers/Predios.php
@@ -303,13 +303,18 @@ class Predios extends CI_Controller {
 	}
 
 	public function ajax_verifica_cod_catastral(){
-		// $cod_catastral = $this->input->post("param1");
-		$reponse = array(
-		               'csrfName' => $this->security->get_csrf_token_name(),
-		               'csrfHash' => $this->security->get_csrf_hash()
-		               );
-		echo json_encode($reponse);
-
+		$cod_catastral = $this->input->get("param1");
+		$this->db->where('codcatas', $cod_catastral);
+		$verifica_cod = $this->db->get('catastro.predio');
+		// print_r($cod_catastral);
+		// print_r($verifica_cod->result());die;
+		if ($verifica_cod->num_rows() > 0) {
+			$respuesta = array('codigo'=>$cod_catastral, 'estado'=>'si');
+			echo json_encode($respuesta);
+		} else {
+			$respuesta = array('codigo'=>$cod_catastral, 'estado'=>'no');
+			echo json_encode($respuesta);
+		}		
 	}
 
 }
