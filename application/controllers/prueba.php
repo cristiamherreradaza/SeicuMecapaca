5251832a45471e9f07229c9d96e1524e1c6dff0c
crisss07 <rodri07crisss@gmail.com>
2019-05-10 17:00:32 -0400

    asig

--- application/controllers/prueba.php
+++ application/controllers/prueba.php
@@ -6,6 +6,8 @@ class Prueba extends CI_Controller {
 	public function __construct() {
         parent:: __construct();
         $this->load->model("AllBloque_model");
+       $this->load->model("inspecciones/inspeccion_model");
+       $this->load->model("rol_model");
     }
 	
 	public function prueba()	
@@ -136,4 +138,54 @@ class Prueba extends CI_Controller {
         $this->load->view('inspecciones/asignacion');
         $this->load->view('admin/footer');
     }
+
+    public function listado(){
+
+        // $this->db->order_by('tramite.derivacion.fec_creacion', 'DESC');
+        $perfil_persona = $this->session->userdata('persona_perfil_id');
+        $datos_persona_perfil = $this->db->get_where('persona_perfil', array('persona_perfil_id'=>$perfil_persona))->result_array();
+        // vdebug($datos_persona_perfil, false, false, true);
+        $datos_organigrama_persona = $this->db->get_where(
+            'tramite.organigrama_persona', 
+            array(
+                'persona_id'=>$datos_persona_perfil[0]['persona_id'],
+                'activo'=>1
+            ))->result_array();
+
+        // vdebug($datos_organigrama_persona, false, false, true);
+        $fuente = $datos_organigrama_persona[0]['organigrama_persona_id'];
+        // vdebug($fuente, false, false, true);
+        $this->db->where('tramite.derivacion.destino', $fuente);
+        $this->db->where('tramite.derivacion.estado', 1);
+        $this->db->order_by('tramite.derivacion.derivacion_id', 'DESC');
+        $query = $this->db->get('tramite.derivacion');
+        // vdebug($query, true, false, true);
+
+        $data['mis_tramites'] = $query->result();
+        $data['verifica'] = $this->rol_model->verifica();
+        //var_dump($usu_creacion);
+
+        $this->load->view('admin/header');
+        $this->load->view('admin/menu');
+        $this->load->view('derivaciones/listado', $data);
+        $this->load->view('admin/footer');
+        $this->load->view('predios/index_js');
+
+    }
+
+    public function lis1(){
+        if($this->session->userdata("login")){
+
+        $lista['verifica'] = $this->rol_model->verifica();
+        $lista['asignacion'] = $this->inspeccion_model->index();
+        $this->load->view('admin/header');
+        $this->load->view('admin/menu');
+        $this->load->view('inspecciones/listado', $lista);
+        $this->load->view('admin/footer');
+        }
+        else{
+            redirect(base_url());
+        }
+    }
+
 }
---
+++ application/views/inspecciones/listado.php
@@ -0,0 +1,74 @@
+
+<!-- ============================================================== -->
+<!-- Start Page Content -->
+<!-- ============================================================== -->
+<div class="page-wrapper">
+    <!-- ============================================================== -->
+    <!-- Container fluid  -->
+    <!-- ============================================================== -->
+    <div class="container-fluid">
+        <!-- ============================================================== -->
+        <!-- Start Page Content -->
+        <!-- ============================================================== -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">INSPECCIONES ASIGNADAS</h4></h4>
+                        <?php //vdebug($mis_tramites, true, false, true); ?>
+                        <table id="tabla_din" class="table table-bordered table-striped" cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th>TRAMITE</th>
+                                    <th>PERSONA</th>
+                                    <th>TIPO_ASIGNACION</th>
+                                    <th>INICIO</th>
+                                    <th>FIN</th>
+                                    <th>ACCION</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($asignacion as $asig): ?>
+                                    <tr>
+                                        <?php $tra = $this->db->query("SELECT tt.correspondencia
+                                                                        FROM tramite.tramite t, tramite.tipo_tramite tt
+                                                                        WHERE t.tramite_id = $asig->tramite_id 
+                                                                        AND t.tipo_correspondencia_id = tt.tipo_correspondencia_id")->row();
+                                            $tra1 = $tra->correspondencia;
+                                          ?>
+                                        <td><?php echo $tra1; ?></td>
+                                        <?php $var = $this->db->query("SELECT *
+                                                                        FROM persona 
+                                                                        WHERE persona_id = $asig->persona_id ")->row();
+                                                $nombres = $var->nombres;
+                                                $paterno = $var->paterno;
+                                          ?>
+                                        <td><?php echo $nombres; ?> <?php echo $paterno; ?></td>
+                                        <?php $var1 = $this->db->query("SELECT *
+                                                                        FROM inspeccion.tipo_asignacion 
+                                                                        WHERE tipo_asignacion_id = $asig->tipo_asignacion_id ")->row();
+                                            $tipo = $var1->tipo;
+                                          ?>
+                                        <td><?php echo $tipo; ?></td>
+                                        <td><?php echo $asig->inicio; ?></td>
+                                        <td><?php echo $asig->fin; ?></td>
+                                        <td>
+                                            <a href="<?php echo base_url(); ?>inspeccion/nuevo/<?php echo $asig->asignacion_id; ?>" class="btn btn-success footable-edit">
+                                                    <span class="fas fa-paper-plane" aria-hidden="true"></span>
+                                            </a>
+                                            
+                                        </td>
+                                        
+                                    </tr>    
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ==============================================================
