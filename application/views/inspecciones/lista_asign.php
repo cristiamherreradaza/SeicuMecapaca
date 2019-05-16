a8be469800840494c89c5e185dc40cda77d961fd
Rodrigo Secko <rodrigosecko@gmail.com>
2019-05-16 16:57:39 -0400

    asignacion

--- application/views/inspecciones/lista_asign.php
+++ application/views/inspecciones/lista_asign.php
@@ -14,7 +14,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">INSPECCIONES</h4></h4>
+                        <h4 class="card-title">ASIGNACIONES</h4></h4>
                         <?php //vdebug($mis_tramites, true, false, true); ?>
                         <table id="tabla_din" class="table table-bordered table-striped" cellspacing="0" width="100%">
                             <thead>
@@ -24,33 +24,26 @@
                                     <th>TIPO ASIGNACION</th>
                                     <th>INICIO</th>
                                     <th>FIN</th>                                    
-                                    <th>acciones</th>
+                                    <th>editar</th>
                                 </tr>
                             </thead>
                           
                             <tbody>
                                 <?php foreach ($lista as $mt): ?>
                                     <tr>
-                                         <td><?php echo $mt->tramite_id; ?></td>
-                                        <td><?php echo $mt->nombres.' '.$mt->paterno.' '.$mt->materno; ?></td>
-                                        
+                                         <td><?php echo $mt->cite; ?></td>
+                                        <td><?php echo $mt->nombres.' '.$mt->paterno.' '.$mt->materno; ?></td>                                        
                                         <td><?php echo $mt->tipo; ?></td>
                                         <td>                                  
                                         <?php echo $mt->inicio; ?>
-                                        <td>
-                                     
+                                        <td>                                     
                                         <?php echo $mt->fin; ?>
-                                        </td>
-                                  
+                                        </td>                                  
                                         <td>   
-                                        <a <?php echo $verifica['baja'];?>="<?= base_url('asignacion/editar/'. $mt->asignacion_id); ?>" type="button" class="btn btn-info footable-delete">
+                                        <a <?php echo $verifica['baja'];?>="<?= base_url('asignacion/edita/'. $mt->asignacion_id); ?>" type="button" class="btn btn-warning footable-delete">
                                                         <span class="fas fas fa-edit" aria-hidden="true">
                                                         </span>
-                                                    </a>
-                                                    <a <?php echo $verifica['baja'];?>="<?= base_url('asignacion/eliminar/'. $mt->asignacion_id); ?>" type="button" class="btn btn-danger footable-delete">
-                                                        <span class="fas fa-trash-alt" aria-hidden="true">
-                                                        </span>
-                                                    </a>
+                                                    </a>                                                   
                                         </td>
                                     </tr>    
                                 <?php endforeach; ?>
@@ -71,14 +64,11 @@
                 <div class="modal-content">
                     <div class="modal-header">
                     <h4 class="modal-title">Acta de inspeccion</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>                        
                     </div>
                     <div class="modal-body">
-
                         <embed src="<?php echo base_url().'public/assets/files/inspeccion/'.$mt->acta_inspeccion; ?>"
                                frameborder="0" width="100%" height="400px">
-
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
                         </div>
